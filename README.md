48723b4c5ea5c13b4be6a43150daed545d61ec09
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-03-03 11:10:10 +0000

    Initial version

---
+++ README.md
@@ -0,0 +1,30 @@
+# opsdroid skill food
+
+A skill for [opsdroid](https://github.com/opsdroid/opsdroid) to interact suggest tasty food.
+
+## Requirements
+
+An API key from [Food2Fork](http://food2fork.com/about/api).
+
+## Configuration
+
+```yaml
+skills:
+  - name: food
+    api-key: "myapikeyfromfood2fork"  # Required
+```
+
+## Usage
+
+#### `What shall I eat?`
+
+Suggests some food from Food2Fork.
+
+> user: What shall I eat?
+>
+> opsdroid: How about chicken fajitas?
+> opsdroid: http://example.com/link-to-recipe-image.jpg
+
+## License
+
+GNU General Public License Version 3 (GPLv3)
---
+++ __init__.py
@@ -0,0 +1,27 @@
+import random
+import aiohttp
+
+from opsdroid.matchers import match_regex
+
+
+@match_regex(r"(w|W)(h)?at (should|shall) (I|i) eat(\?)?")
+async def whatshallieat(opsdroid, config, message):
+
+    listoffood = [
+        "How should I know I'm a machine!",
+        "How about a packet of crisps!",
+        "What about some sandwiches?",
+        "You should have karela in your curry."
+    ]
+
+    url = "http://food2fork.com/api/search?key={}&q=".format(config.get("api-key"))
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                recipes = await resp.json()
+                my_recipe = random.choice(recipes["recipes"])
+                await message.respond("How about {}?".format(my_recipe["title"].lower()))
+                await message.respond(my_recipe["image_url"])
+            else:
+                await message.respond(random.choice(listoffood))
